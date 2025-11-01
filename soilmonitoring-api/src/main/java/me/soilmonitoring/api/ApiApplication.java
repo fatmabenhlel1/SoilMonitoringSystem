15224e91d5f71acdad2e81d4b30508cef73f5118
fatmabenhlel1 <fatmaezzahra.benhelal@supcom.tn>
2025-11-01 15:40:25 +0100

    added api structure

---
+++ soilmonitoring-api/src/main/java/me/soilmonitoring/api/ApiApplication.java
@@ -0,0 +1,12 @@
+package me.soilmonitoring.api;
+
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+
+@ApplicationPath("/rest-iam")
+public class ApiApplication extends Application {
+
+    }
+
